1efc05de01e6cc04c1249a9020f6b8d1cc0de8b4
Michele de Lima <michele.delima@ambevtech.com.br>
2023-04-17 20:01:02 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Adega
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StartMenu();
+        }
+
+        public static void StartMenu()
+        {
+            Console.Title = "Projeto Adega";
+           
+            Console.WriteLine("Escolha qual area deseja realizar operação: ");
+            while (true)
+            {
+                Console.WriteLine("1. Cliente");
+                Console.WriteLine("2. Produto");
+                Console.WriteLine("3. Estoque");
+                Console.WriteLine("0. Sair");
+                Console.Write("Opção: ");
+                switch (Console.Read())
+                {
+                    case '1':
+                        //chama start que é o "menu de operações" do cliente
+                        break;
+                    case '2':
+                        //chama start que é o ""menu de operações"" do produto
+                        break;
+                    case '3':
+                        //chama start que é o ""menu de operações"" do estoque
+                        break;
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.ReadLine();
+                        Console.WriteLine("Opção Invalida, Tente novamente.....");
+                        break;
+                }
+            }
+        }
+    }
+}
